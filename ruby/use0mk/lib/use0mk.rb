0aba310d7f2ad906b39c2423a35c08256214ade0
hf <s.dimitrovski@gmail.com>
2010-12-09 15:03:56 +0100

    Updates, Docs, And Other Tales of The Universe

--- ruby/use0mk/lib/use0mk.rb
+++ ruby/use0mk/lib/use0mk.rb
@@ -26,132 +26,168 @@ require "json"
 module Use0MK
   SHORTEN_URI = "http://api.0.mk/v2/skrati"
   PREVIEW_URI = "http://api.0.mk/v2/pregled"
-
   ORIGINS = [:shorten, :preview]
 
   # ERRORS defined below
 end
 
-class Use0MK::RedirectError < StandardError
+# Standard error class concerning {Use0MK} errors.
+class Use0MK::Error < StandardError
+  # the message returned by 0.mk
+  # @return [String]
   attr_reader :message
+  # the status ID for the error
+  # @return [Integer]
+  attr_reader :status
+end
 
+# Too many redirects while attempting an API call.
+#
+# *status*: 100
+class Use0MK::RedirectError < Use0MK::Error
   def initialize(message)
     @message = message
+    @status = 100
   end
 end
 
-class Use0MK::EmptyLinkError < StandardError
-  attr_reader :message
+# Provided link (any URI concerning any API call) was empty.
+#
+# *status*: 1
+class Use0MK::EmptyLinkError < Use0MK::Error
   def initialize(message)
     @message = message
-  end
-
-  def status
-    return 1
+    @status = 1
   end
 end
 
-class Use0MK::InvalidLinkError < StandardError
-  attr_reader :message
-
+# Provided link (any URI concerning any API call) was invalid.
+#
+# *status*: 2
+class Use0MK::InvalidLinkError < Use0MK::Error
   def initialize(message)
     @message = message
-  end
-
-  def status
-    return 2
+    @status = 2
   end
 end
 
-class Use0MK::InvalidFormatError < StandardError
-  attr_reader :message
-
+# Provided format parameter was invalid. Probably an internal error.
+#
+# *status*: 3
+class Use0MK::InvalidFormatError < Use0MK::Error
   def initialize(message)
     @message = message
-  end
-
-  def status
-    return 3
+    @status = 3
   end
 end
 
-class Use0MK::Invalidshort_nameError < StandardError
-  attr_reader :message
-
-  def initialize(message)
+# Provided (custom) short name was invalid.
+#
+# *status*: 4
+class Use0MK::InvalidShortNameError < Use0MK::Error
+ def initialize(message)
     @message = message
-  end
-
-  def status
-    return 4
+    @status = 4
   end
 end
 
-class Use0MK::InvalidAPIKey < StandardError
-  attr_reader :message
-
+# Provided API key was invalid.
+#
+# *status*: 5
+class Use0MK::InvalidAPIKey < Use0MK::Error
   def initialize(message)
     @message = message
-  end
-
-  def status
-    return 5
+    @status = 5
   end
 end
 
-class Use0MK::InvalidCredentials < StandardError
-  attr_reader :message
-
+# Provided credentials were invalid.
+#
+# *status*: 6
+class Use0MK::InvalidCredentials < Use0MK::Error
   def initialize(message)
     @message = message
-  end
-
-  def status
-    return 6
+    @status = 6
   end
 end
 
-class Use0MK::InvalidAPICall < StandardError
-  attr_reader :message
-
+# API call not supported by 0.mk. Check {Use0MK::SHORTEN_URI} and
+# {Use0MK::PREVIEW_URI} to see which API is being used. Probably an
+# internal error.
+#
+# *status*: 7
+class Use0MK::InvalidAPICall < Use0MK::Error
   def initialize(message)
     @message = message
-  end
-
-  def status
-    return 7
+    @status = 7
   end
 end
 
 module Use0MK
-  ERRORS = {
-    1 => Use0MK::EmptyLinkError,
-    2 => Use0MK::InvalidLinkError,
-    3 => Use0MK::InvalidFormatError,
-    4 => Use0MK::Invalidshort_nameError,
-    5 => Use0MK::InvalidAPIKey,
-    6 => Use0MK::InvalidCredentials,
-    7 => Use0MK::InvalidAPICall
-  }
+  APIERRORS = [
+    Use0MK::EmptyLinkError,
+    Use0MK::InvalidLinkError,
+    Use0MK::InvalidFormatError,
+    Use0MK::InvalidShortNameError,
+    Use0MK::InvalidAPIKey,
+    Use0MK::InvalidCredentials,
+    Use0MK::InvalidAPICall
+  ]
 end
 
+# Represents a shortened URI from 0.mk. It contains only the information
+# retrieved by the API call to 0.mk. The volume of information with any
+# API call is not consistent therefore any attribute of this class that
+# is +nil+ or +empty+ should be ignored.
+#
+# The origin (the API call which generated the object of this class)
+# of this object can be tracked with the +origin+ attribute. The origin
+# is a Symbol which is a member of {Use0MK::ORIGINS}.
+#
+# It is not a good idea to create instances of this class by hand, as
+# it contains only data available from API calls to 0.mk.
+#
 class Use0MK::ShortenedURI
+  # a member of {Use0MK::ORIGINS}
+  # @return [Symbol]
   attr_reader :origin
+  # the long URI
+  # @return [String, nil]
   attr_reader :long_uri
+  # the shortened URI
+  # @return [String, nil]
   attr_reader :short_uri
+  # the short name (+http://0.mk/short_name+)
+  # @return [String, nil]
   attr_reader :short_name
+  # the stats URI
+  # @return [String, nil]
   attr_reader :stats_uri
+  # the document title of the long URI
+  # @return [String, nil]
   attr_reader :title
+  # the deletion URI
+  # @return [String, nil]
   attr_reader :delete_uri
+  # the deletion code
+  # @return [String, nil]
   attr_reader :delete_code
+  # whether this URI is deleted
+  # @return [true, false]
+  attr_reader :deleted
 
-  def initialize(origin, attributes = {:long_uri => nil, :short_uri => nil, :short_name => nil, :stats_uri => nil, :title => nil, :delete_uri => nil, :delete_code => nil})
+  # Initializes this class. Usually called from either +shorten+ or +preview+.
+  #
+  # @param [Symbol] origin origin of this class (which API call created it), a member of {Use0MK::ORIGINS}
+  # @param [Hash] attributes hash of the attributes needed to create the object
+  def initialize(origin, attributes = {})
 
     if !(Use0MK::ORIGINS.member? origin)
       raise ArgumentError, "origin should be a member of Use0MK::ORIGINS"
     end
 
     @origin = origin
+    @deleted = @origin != :shorten
 
     @long_uri = attributes[:long_uri]
     @short_uri = attributes[:short_uri]
@@ -162,24 +198,45 @@ class Use0MK::ShortenedURI
     @delete_code = attributes[:delete_code]
   end
 
+  # Deletes the shortened URI.
+  # This method calls {Use0MK::Interface.delete} internally.
+  #
+  # @return [true, false] wheter deletion succeeds (usually +true+ when +origin+ is +:shorten+)
   def delete
-    if @origin == :preview
+    if @deleted
       return false
     end
 
-    Use0MK::Interface.delete(@delete_uri, @delete_code)
+    @deleted = Use0MK::Interface.delete(@delete_uri, @delete_code)
   end
 end
 
+# This class represents the main interface through which one can operate on
+# the 0.mk API.
 class Use0MK::Interface
+  # the username associated with 0.mk
+  # @return [String, nil]
   attr_accessor :username
+  # the API key for 0.mk
+  # @return [String, nil]
   attr_accessor :apikey
 
+  # Initializes the {Use0MK::Interface}.
+  #
+  # @param [String] username 0.mk username
+  # @param [String] apikey 0.mk API key
   def initialize(username = nil, apikey = nil)
     @username = username
     @apikey = apikey
   end
 
+  # Deletes a shortened URI from 0.mk.
+  # This method does not raise or handles any exceptions. It also does not
+  # handle redirection.
+  #
+  # @param [String] delete_uri the 0.mk URI to delete
+  # @param [String] delete_code the 0.mk delete code associated with the 0.mk URI
+  # @return [true, false] wheter the delete succeeded
   def self.delete(delete_uri, delete_code)
     delete_uri = URI.parse(delete_uri)
 
@@ -189,19 +246,38 @@ class Use0MK::Interface
     case response
       when Net::HTTPSuccess
         return true # phew, that was hard work, that delete
-      else
-        response.error!
       end
 
     return false
   end
 
+  # Shortens a URI.
+  # This method can raise several exceptions of which most importantly:
+  # * {URI::Error}
+  # * {Net::HTTPException}
+  # * {Use0MK::Error}
+  #
+  # It is a wise thing to watch for any of those exceptions.
+  #
+  # @param [String] uri the _long_ URI to shorten
+  # @param [String] short_name a custom short name to use when shortening the long URI (+http://0.mk/short_name+)
+  # @return [Use0MK::ShortenedURI] the shortened URI
   def shorten(uri, short_name = nil)
     uri = URI.parse(uri)
 
     _shorten(uri, short_name)
   end
 
+  # Inspects an already shortened URI.
+  # This method can raise several exceptions of which most importantly:
+  # * {URI::Error}
+  # * {Net::HTTPException}
+  # * {Use0MK::Error}
+  #
+  # It is a wise idea to watch for any of these.
+  #
+  # @param [Hash] link provide +:short_name+ to inspect only the short name of an URI, or +:uri+ to inspect a whole +http://0.mk/short_name+ URI
+  # @return [Use0MK::ShortenedURI] the shortened URI (volume of data varies)
   def preview(link = {:short_name => nil, :uri => nil})
     uri = nil
 
@@ -261,7 +337,7 @@ class Use0MK::Interface
 
   def _parse(origin, json)
     if json['status'] != 1
-      raise Use0MK::ERRORS[json['greskaId']], json['greskaMsg'].to_s.strip
+      raise Use0MK::APIERRORS[json['greskaId'].to_i - 1], json['greskaMsg'].to_s.strip
     end
 
     assign = {
